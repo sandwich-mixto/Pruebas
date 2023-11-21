4ef3b28a561a554330e97f511c4780097da0cdf3
Jorjime <jorjime9@gmail.com>
2023-11-21 10:47:30 +0100

    Prueba con este :D

---
+++ Cacatua.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Cacatua {
+    public static void main(String[] args) {
+        Scanner teclao = new Scanner(System.in);
+        int num1 = teclao.nextInt();
+        int num2 = teclao.nextInt();
+        System.out.println(num2 + num1);
+    }
+}
