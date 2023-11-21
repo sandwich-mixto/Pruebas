f2a0d950b3f7f6dc04467d517b7ad849624b4782
Jorjime <jorjime9@gmail.com>
2023-11-21 10:35:39 +0100

    Añadir

--- PrimerCodigo.java
+++ PrimerCodigo.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class PrimerCodigo {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
+        System.out.println("Escribe tu nombre xd");
         String nombre = teclado.nextLine();
         System.out.println("Enhorabuena, te llamas " + nombre);
     }
