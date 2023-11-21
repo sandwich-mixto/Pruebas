be36cf4298af1017658da8854c60d4e575e6a772
sandwich-mixto <151618689+sandwich-mixto@users.noreply.github.com>
2023-11-21 10:22:21 +0100

    Add files via upload

---
+++ PrimerCodigo.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class PrimerCodigo {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        String nombre = teclado.nextLine();
+        System.out.println("Enhorabuena, te llamas " + nombre);
+    }
+}
